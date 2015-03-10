b7b55fe519a061d5a5e3a4453d0170f08b5c1d03
Nick Shelley <nickmshelley@gmail.com>
2015-03-10 08:36:06 -0600

    Add search bar and collection view.

---
+++ SearchShindig/ThumbnailCell.swift
@@ -0,0 +1,13 @@
+//
+//  ThumbnailCell.swift
+//  SearchShindig
+//
+//  Created by Heather Shelley on 3/10/15.
+//  Copyright (c) 2015 Mine. All rights reserved.
+//
+
+import UIKit
+
+class ThumbnailCell: UICollectionViewCell {
+
+}
